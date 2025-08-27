9ba667d4135f71dc4a7134755edf3f9cdbec0e3c
Daniel Manuel <daniel.manuel@angolacables.co.ao>
2025-08-27 15:03:20 +0100

    finalizado front-end

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,16 @@ module.exports = {
     "./src/**/*.{html,ts}"
   ],
   theme: {
-    extend: {},
+    extend: {
+       screens: {
+      'xs': '320px', // 👈 adicionando o breakpoint
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
   },
   plugins: [],
+}
 }
