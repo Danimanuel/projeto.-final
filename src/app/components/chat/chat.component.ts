398f0323acf41112860f722b158334306f6a1e39
Daniel Manuel <daniel.manuel@angolacables.co.ao>
2025-08-27 13:38:40 +0100

    tornando Responsivo

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,11 +1,23 @@
+import { NgClass, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-chat',
-  imports: [],
+  imports: [NgClass, NgIf],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.css'
 })
 export class ChatComponent {
 
+  mostraChat = true;
+mostrarChat: any;
+
+  appear() {
+    this.mostraChat = !this.mostraChat
+  }
+
 }
+
+
